21048c627529c1507eb269b646426177c1abdc62
lamarquesita <69827616+lamarquesita@users.noreply.github.com>
2023-10-17 16:57:49 -0400

    Initial commit

---
+++ src/screens/Propuesta/Propuesta.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+export const Propuesta = () => {
+  return (
+    <div className="propuesta">
+      <div className="div-elements-wrapper">
+        <div className="div-elements">
+          <button className="button">
+            <div className="install-app">INSTALL APP</div>
+          </button>
+          <div className="link">
+            <div className="launch-beta">LAUNCH BETA</div>
+          </div>
+          <div className="launch-beta-wrapper">
+            <div className="text-wrapper">LAUNCH BETA</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="div">LAUNCH BETA</div>
+          </div>
+          <div className="link-2">
+            <div className="div">LAUNCH BETA</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
